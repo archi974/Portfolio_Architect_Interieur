78006864f47d9d9c1ef37a76328bf40c91f612fe
Vincent <jeremy.kerbidi@gmail.com>
2023-03-01 12:58:16 +0100

    [UPDATE] modal with picture and close button

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,3 +1,5 @@
+// import "./login";
+
 document.addEventListener('DOMContentLoaded', function (event) {
     event.preventDefault();
     let gallery = document.getElementById('gallery');
@@ -9,10 +11,10 @@ document.addEventListener('DOMContentLoaded', function (event) {
                 let imageUrl = imageInfo.imageUrl;
                 let categoryId = imageInfo.categoryId;
                 gallery.innerHTML += `
-            <figure class="project_item" data-item-category="${categoryId}">
-            <img src="${imageUrl}" alt="${title}">
-            <figcaption>${title}</figcation>
-            </figure>`;
+                <figure class="project_item" data-item-category="${categoryId}">
+                <img src="${imageUrl}" alt="${title}">
+                <figcaption>${title}</figcation>
+                </figure>`;
             });
             const buttonFilter = document.querySelectorAll('.container_filter li');
             const projectsItem = document.querySelectorAll('.project_item');
@@ -23,6 +25,9 @@ document.addEventListener('DOMContentLoaded', function (event) {
                     [...projectsItem].forEach(item => {
                         const projectItemCategory = item.dataset.itemCategory;
                         if (filterButtonCategory == 0 || projectItemCategory === filterButtonCategory) {
+                            if (filterButtonCategory != 0) {
+                                buttonFilter[0].children[0].setAttribute('class', 'button_filter');
+                            }
                             item.style.display = "initial";
                         } else {
                             item.style.display = "none";
@@ -30,14 +35,40 @@ document.addEventListener('DOMContentLoaded', function (event) {
                     })
                 });
             });
+            const modal = document.getElementById('edit').appendChild(document.createElement("div"));
+            document.getElementById('projectEdit').addEventListener('click', (e) => {
+                e.preventDefault();
+                modal.setAttribute('class', 'modal');
+                modal.appendChild(document.createElement('div')).setAttribute('id', 'modal_content')
+                document.getElementById('modal_content').innerHTML =
+                    `<div class="closeModal"><i class="fa-solid fa-xmark" id="closeModal"></i></div>
+                    <h2>Galerie photo</h2>
+                    <div id="contentImgModal"></div>`
+                response.forEach(modalImageInfo => {
+                    let imageUrl = modalImageInfo.imageUrl;
+                    let title = modalImageInfo.title;
+                    document.getElementById('contentImgModal').innerHTML += 
+                    `<div class="editImgModal">
+                        <img src=${imageUrl} class="imgModal" alt="${title}">
+                    </div>`
+                })
+                // let iconeArrowMoove = document.querySelector('.editImgModal').insertBefore(document.createElement('i'), document.getElementsByClassName('imgModal').firstChild)
+                // iconeArrowMoove.setAttribute('class', 'fa-regular fa-arrows-up-down-left-right')
+                document.getElementById('closeModal').addEventListener('click', (e) => {
+                    e.preventDefault();
+                    let buttonEdit = document.getElementById('edit');
+                    buttonEdit.lastChild.replaceChildren();
+                    buttonEdit.lastChild.classList.remove('modal');
+                });
+            });
         });
     let getCookie = document.cookie.split('=');
     if (getCookie[0] === "loginToken" && getCookie[1].length === 143) {
         let editComponent = document.getElementById('edit');
         editComponent.innerHTML =
-        `<i class="fa-regular fa-pen-to-square"></i>
-        <p>Mode édition</p>
-        <button type="">publier les changements</button>`
+            `<i class="fa-regular fa-pen-to-square"></i>
+            <p>Mode édition</p>
+            <button type="">publier les changements</button>`
         editComponent.style.display = 'flex';
         document.querySelector('nav li:nth-child(3)').innerHTML = `<a href="./" id="disconnect">Logout</a>`;
         document.getElementById('disconnect').addEventListener('click', function (event) {
@@ -45,13 +76,13 @@ document.addEventListener('DOMContentLoaded', function (event) {
             document.cookie = 'loginToken=;Max-age=0;';
             location.reload();
         });
-        
+
         const arrayId = [
             document.getElementById('introduction').firstElementChild,
             document.getElementById('introduction').lastElementChild,
             document.getElementById('portfolio').firstElementChild
         ]
-        for (let i = 0; i <= arrayId.length-1; i++) {
+        for (let i = 0; i <= arrayId.length - 1; i++) {
             let id = ["profileImgEdit", "textDescriptionEdit", "projectEdit"]
             let editClass = document.getElementsByClassName('edit');
             arrayId[i].insertBefore(document.createElement('a'), arrayId[i].lastChild).setAttribute('class', 'edit')
@@ -59,14 +90,6 @@ document.addEventListener('DOMContentLoaded', function (event) {
             editClass[i].style.cursor = 'pointer';
             editClass[i].innerHTML = '<i class="fa-regular fa-pen-to-square"></i><p>modifier</p>';
         }
-
-        const modal = document.getElementById('edit').appendChild(document.createElement("div"));
-        document.getElementById('projectEdit').addEventListener('click', (e) => {
-            modal.setAttribute('class', 'modal');
-            modal.appendChild(document.createElement('div')).setAttribute('id', 'modal_content')
-            document.getElementById('modal_content').innerHTML = 
-            `<i class="fa-regular fa-arrow-left"></i>
-            `
-        })
+        // <i class="fa-solid fa-arrow-left-long"></i>
     }
 });

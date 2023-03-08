499fe3ada5c2c1480ce7c65b73a3dc7970fc8a99
Vincent <jeremy.kerbidi@gmail.com>
2023-03-08 18:57:34 +0100

    [ADD] delete fetch

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -52,12 +52,14 @@ document.addEventListener('DOMContentLoaded', function (event) {
                     `<div class="closeModal">
                         <i class="fa-solid fa-xmark" id="closeModal"></i>
                     </div>
-                    <h2>Galerie photo</h2>
-                    <div id="contentImgModal"></div>
-                    <hr>
-                    <div class="addAndRemoveImg">
-                        <input type="button" value="Ajouter une photo" id="buttonAddPicture">
-                        <a id="deleteAllImg">Supprimer la galerie</a>
+                    <div class="galleryContent">
+                        <h2>Galerie photo</h2>
+                        <div id="contentImgModal"></div>
+                        <hr>
+                        <div class="addAndRemoveImg">
+                            <input type="button" value="Ajouter une photo" id="buttonAddPicture">
+                            <a id="deleteAllImg">Supprimer la galerie</a>
+                        </div>
                     </div>`
 
                 // image modal loop with logo
@@ -70,7 +72,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
                                 <img src=${imageUrl} alt="${title}">
                                 <div class="logoModal">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="mooveLogoModal"><path d="M278.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-64 64c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l9.4-9.4V224H109.3l9.4-9.4c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-64 64c-12.5 12.5-12.5 32.8 0 45.3l64 64c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-9.4-9.4H224V402.7l-9.4-9.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l64 64c12.5 12.5 32.8 12.5 45.3 0l64-64c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-9.4 9.4V288H402.7l-9.4 9.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l64-64c12.5-12.5 12.5-32.8 0-45.3l-64-64c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l9.4 9.4H288V109.3l9.4 9.4c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-64-64z"/></svg>
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"/></svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="deleteLogoModal"><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"/></svg>
                                 </div>
                             </div>
                             <p>éditer</p>
@@ -82,6 +84,22 @@ document.addEventListener('DOMContentLoaded', function (event) {
                     buttonEdit.lastChild.replaceChildren();
                     buttonEdit.lastChild.classList.remove('modal');
                 });
+                let arrayDeleteImgButton = document.getElementsByClassName('deleteLogoModal');
+                for (let i = 0; i < arrayDeleteImgButton.length; i++) {
+                    const DeleteImgButton = arrayDeleteImgButton[i];
+                    DeleteImgButton.addEventListener('click', (e) => {
+                        e.preventDefault();
+                        console.log(e);
+                        // fetch(`http://localhost:5678/api/works/${id}`, {method: 'DELETE'})
+                        //     .then(response => response.json())
+                        //     .then(response => console.log(response))
+
+                    });
+                }
+                let addPicture = document.getElementById('buttonAddPicture');
+                addPicture.addEventListener('click', (e) => {
+                    
+                })
             });
         });
 
@@ -116,5 +134,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
             editClass[i].innerHTML = '<i class="fa-regular fa-pen-to-square"></i><p>modifier</p>';
         }
         // <i class="fa-solid fa-arrow-left-long"></i>
+        // delete just 1 image
+
     }
 });

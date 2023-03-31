18143b3a47cde6a581fed0244809eded03bc084f
Vincent <vincent.k.97480@gmail.com>
2023-03-31 17:22:50 +0200

    [UPDATE] error message

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,27 +1,29 @@
 document.getElementById('buttonSubmit').addEventListener('click', async function (event) {
     event.preventDefault();
-    let formData = {"email": document.myForm.email.value, "password": document.myForm.password.value}
+    const email = document.myForm.email.value;
+    const password = document.myForm.password.value;
+    const formData = {"email": email, "password": password}
     try {
-        let response = await fetch("http://localhost:5678/api/users/login", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            },
-            body: JSON.stringify(formData),
-    
-        });
-        let result = await response.json();
-        switch (response.status) {
-            case 200:
-                document.cookie = `loginToken=${result.token}`;
-                location.replace("http://127.0.0.1:5500/FrontEnd/");
-            break;
-            case 404:
-                document.getElementById("errorEmail").innerHTML = "Votre E-mail ne correspond pas.";
-            break;
-            case 401:
-                document.getElementById("errorPassword").innerHTML = "Votre mot de passe ne correspond pas.";
-            break;
+        if (email && password) {
+            let response = await fetch("http://localhost:5678/api/users/login", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+                body: JSON.stringify(formData),
+        
+            });
+            let result = await response.json();
+            switch (response.status) {
+                case 200:
+                    document.cookie = `loginToken=${result.token}`;
+                    location.replace("http://127.0.0.1:5500/FrontEnd/");
+                break;
+                default:
+                    document.getElementById("errorMessageLogin").innerHTML = "Votre E-mail ou votre mot de passe ne correspond pas.";
+            }
+        } else {
+            document.getElementById("errorMessageLogin").innerHTML = "Veuillez remplir les champs Email et Mot de passe.";
         }
     } catch (error) {
         console.error(error);
